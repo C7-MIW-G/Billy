432da83f8128a6d94170ee85182a04ade8121151
LarsS96 <93921723+LarsS96@users.noreply.github.com>
2022-01-26 15:44:52 +0100

    all files added

---
+++ src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/BillyApplication.java
@@ -0,0 +1,13 @@
+package nl.miw.se.cohort7.eindproject.rise.billy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miw/se/cohort7/eindproject/rise/billy/BillyApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miw.se.cohort7.eindproject.rise.billy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
