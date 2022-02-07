a337c748906ab98f4509b2313f112e363f460e42
LarsS96 <93921723+LarsS96@users.noreply.github.com>
2022-02-07 16:41:47 +0100

    BY-4 userOverview added

--- src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/model/User.java
+++ src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/model/User.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * @author Lars van der Schoor <la.van.der.schoor@st.hanze.nl>
@@ -22,13 +24,9 @@ public class User {
     @GeneratedValue
     private long userId;
 
-    private String firstName;
+    private String userName;
 
-    private String infixName;
-
-    private String lastName;
-
-    private LocalDateTime dateOfBirth;
+    private Date dateOfBirth;
 
     private double credit;
-}
+    }
