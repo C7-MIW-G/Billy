1d7960338b299bf70627ca4294582d14caa5326e
LarsS96 <93921723+LarsS96@users.noreply.github.com>
2022-02-08 12:46:56 +0100

    BY-56 introduced login security

--- src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/model/User.java
+++ src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/model/User.java
@@ -3,10 +3,18 @@ package nl.miw.se.cohort7.eindproject.rise.billy.model;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author Lars van der Schoor <la.van.der.schoor@st.hanze.nl>
@@ -17,15 +25,48 @@ import java.util.Date;
 @Entity
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
     private long userId;
 
-    private String userName;
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String password;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
 
-    private Date date;
+    private Date birthdate;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+
+        authorityList.add(new SimpleGrantedAuthority("ROLE_BARTENDER"));
+
+        return authorityList;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/repository/UserRepository.java
+++ src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/repository/UserRepository.java
@@ -3,5 +3,8 @@ package nl.miw.se.cohort7.eindproject.rise.billy.repository;
 import nl.miw.se.cohort7.eindproject.rise.billy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
