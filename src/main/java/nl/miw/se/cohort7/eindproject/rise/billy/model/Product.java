ffb1108e4b34d176617d98fa4164fb2dec601e2d
Jordy <j.pragt@st.hanze.nl>
2022-02-28 09:10:57 +0100

    fixed product equals

--- src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/model/Product.java
+++ src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/model/Product.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Objects;
 
 /**
  * @author Martijn Gäbler <m.gabler@st.hanze.nl>
@@ -36,6 +37,18 @@ public class Product {
     @ManyToOne
     private Category category;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId);
+    }
 }
 
 
