6b94d5cc55cf1485583780fd6713a88bdf95dbbd
LarsS96 <93921723+LarsS96@users.noreply.github.com>
2022-03-04 14:38:09 +0100

    script update

--- sql/initdb.sql
+++ sql/initdb.sql
@@ -1,7 +1,7 @@
 -- First drop any existing database and user
-DROP DATABASE IF EXISTS billy;
-DROP USER IF EXISTS 'userBilly'@'localhost';
+DROP DATABASE IF EXISTS Billy;
+DROP USER IF EXISTS 'userBilly'@'%';
 
 create database billy; -- Creates the new database
-create user 'userBilly'@'localhost' identified by 'pwBilly'; -- Creates the user
-grant all on billy.* to 'userBilly'@'localhost'; -- Gives all privileges to the new user on the newly created database
+create user 'userBilly'@'%' identified by 'pwBilly'; -- Creates the user
+grant all on billy.* to 'userBilly'@'%'; -- Gives all privileges to the new user on the newly created database
