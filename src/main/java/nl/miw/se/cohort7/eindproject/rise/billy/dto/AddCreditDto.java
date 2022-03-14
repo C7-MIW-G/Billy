15634429be3124d19b4d000c07b81c491f8612a7
joris <Joris_bij@hotmail.com>
2022-03-14 16:53:03 +0100

    BY-29 Users account balance can be updated

---
+++ src/main/java/nl/miw/se/cohort7/eindproject/rise/billy/dto/AddCreditDto.java
@@ -0,0 +1,15 @@
+package nl.miw.se.cohort7.eindproject.rise.billy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * auditor: Joris van der Bij Email: j.vanderbij@hanze.st.nl
+ */
+
+@Getter
+@Setter
+public class AddCreditDto {
+
+    private double value;
+}
